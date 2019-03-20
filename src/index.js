0ddcda48a4aaa4c58e5e7ffe8c1bb13cf90afd68
Jose Luis Delgado <joludelgar@gmail.com>
2019-03-20 10:23:57 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment);
+  return (
+    <button class="btn" onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display(props) {
+  return (
+    <div id="display">
+      <span id="text">{props.message}</span>
+    </div>
+  );
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = incrementValue =>
+    setCounter(counter + incrementValue);
+  return (
+    <div>
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={100} />
+      <Display message={counter} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
